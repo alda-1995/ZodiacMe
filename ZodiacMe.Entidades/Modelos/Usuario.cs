84f9c2ef702dc7340e3d2ccb0113af117d5b12a4
alda-1995 <aldair@canalla.agency>
2022-10-30 16:41:35 -0600

    .

---
+++ ZodiacMe.Entidades/Modelos/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZodiacMe.Entidades.Modelos
+{
+    public class Usuario
+    {
+        public Usuario()
+        {
+            UsuarioId = Guid.NewGuid();
+        }
+        public Guid UsuarioId { get; set; }
+        [Required]
+        public string Correo { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+        public byte RolId { get; set; }
+        public Rol Rol { get; set; }
+
+    }
+}
